d5f1848f40bc336e4745b4036f29d3de3cdd3583
Angela Carrillo <angelacarrv@icloud.com>
2026-04-08 11:49:52 -0400

    Redid the Main menu to look more accurate to Hi-fi

--- crackedExample/classicMode/MainMenu.swift
+++ crackedExample/classicMode/MainMenu.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Angela on 2/4/26 amat.
 
-
 import SwiftUI
 
 
@@ -15,55 +14,117 @@ struct  MainMenu: View {
 
     
     var body: some View {
-        NavigationStack{
-            VStack{
-                Button("Classic Mode") {
-                    startClassicMode = true
+        GeometryReader  { geo in
+            NavigationStack{
+                ZStack {
+                    Image("background")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                        .frame(width:geo.size.width, height:geo.size.height,alignment: .center)
+                    //insert background here
+                    VStack{
+                        Spacer()
+                        Button("Classic Mode") {
+                            startClassicMode = true
+                        }
+                        .padding(30)
+                        .frame(maxWidth: .infinity)
+                        .font(Font.custom("Super Meatball", size: 20))
+                        .background(Color.customBeige)
+                        .shadow(radius: 5)
+                        .foregroundStyle(Color.black)
+                        .clipShape(Capsule())
+                        .overlay{
+                            Capsule()
+                                .stroke(Color.black, lineWidth: 0.8)
+                        }
+                        .shadow(radius: 5)
+                        .navigationDestination(isPresented: $startClassicMode) {
+                            GameView()
+                                .navigationBarBackButtonHidden(true)
+                            
+                        }
+                        
+                        
+                        Button("Zen Mode") {
+                            startZenMode = true
+                        }
+                        .padding(30)
+                        .frame(maxWidth: .infinity)
+                        .font(Font.custom("Super Meatball", size: 20))
+                        .background(Color.customBeige)
+                        .shadow(radius: 5)
+                        .foregroundStyle(Color.black)
+                        .clipShape(Capsule())
+                        .overlay{
+                            Capsule()
+                                .stroke(Color.black, lineWidth: 0.8)
+                        }
+                        .shadow(radius: 5)
+                        .navigationDestination(isPresented: $startZenMode) {
+                            GameView()
+                        }
+                        
+                        HStack{
+                            Button(action: {}){
+                                Image(systemName: "gearshape.fill")
+                                    .accessibilityLabel("Access settings.")
+                            }
+                            .padding()
+                            
+                            .background(Color.customBeige)
+                            .shadow(radius: 5)
+                            .foregroundStyle(Color.black)
+                            .clipShape(Circle())
+                            .overlay{
+                                Circle()
+                                    .stroke(Color.black, lineWidth: 0.8)
+                            }
+                            .shadow(radius: 5)
+                            .navigationDestination(isPresented: $startZenMode) {
+                                GameView()
+                            }
+                            Button(action: {}){
+                                Image(systemName: "storefront.fill")
+                                    .accessibilityLabel("Access the store.")
+                            }
+                            .padding()
+                            .background(Color.customBeige)
+                            .shadow(radius: 5)
+                            .foregroundStyle(Color.black)
+                            .clipShape(Circle())
+                            .overlay{
+                                Circle()
+                                    .stroke(Color.black, lineWidth: 0.8)
+                            }
+                            .shadow(radius: 5)
+                            .navigationDestination(isPresented: $startZenMode) {
+                                GameView()
+                            }
+                            Button(action: {}){
+                                Image(systemName: "crown.fill")
+                                    .accessibilityLabel("Access the global and daily leaderboards.")
+                            }
+                            .padding()
+                            .background(Color.customBeige)
+                            .shadow(radius: 5)
+                            .foregroundStyle(Color.black)
+                            .clipShape(Circle())
+                            .fixedSize()
+                            .overlay{
+                                Circle()
+                                    .stroke(Color.black, lineWidth: 0.8)
+                            }
+                            .shadow(radius: 5)
+                            .navigationDestination(isPresented: $startZenMode) {
+                                GameView()
+                            }
+                            
+                        }
+                    }
+                    .fixedSize(horizontal: true, vertical: false)
                 }
-                .padding()
-                .font(Font.custom("Super Meatball", size: 20))
-                .background(Color.customBeige)
-                .shadow(radius: 5)
-                .border(Color(.black), width: 0.8)
-                .foregroundStyle(Color.black)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .shadow(radius: 5)
-                .navigationDestination(isPresented: $startClassicMode) {
-                    GameView()
-                }
-                
-                Button("Zen Modee") {
-                    startZenMode = true
-                }
-                .padding()
-                .font(Font.custom("Super Meatball", size: 20))
-                .background(Color.customBeige)
-                .shadow(radius: 5)
-                .border(Color(.black), width: 0.8)
-                .foregroundStyle(Color.black)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .shadow(radius: 5)
-                .navigationDestination(isPresented: $startZenMode) {
-                    GameView()
-                }
-                
-//                HStack{
-//                    Button(){
-//
-//                    }
-//                    .padding()
-//                    .background(Color.customBeige)
-//                    .shadow(radius: 5)
-//                    .border(Color(.black), width: 0.8)
-//                    .foregroundStyle(Color.black)
-//                    .clipShape(RoundedRectangle(cornerRadius: 15))
-//                    .shadow(radius: 5)
-//                    .navigationDestination(isPresented: $startZenMode) {
-//                        GameView()
-//                    }
-//
-//
-//                }
             }
         }
     }
@@ -71,8 +132,3 @@ struct  MainMenu: View {
 #Preview (){
     MainMenu()
 }
-
-
-
-
-
